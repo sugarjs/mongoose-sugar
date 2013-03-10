a715d94a919bf1fd491e9aa175f62606c397f9fb
Juho Vepsalainen <bebraw@gmail.com>
2013-03-10 13:39:17 +0200

    Define LibraryAuthor schema for testing

--- tests/models.js
+++ tests/models.js
@@ -22,3 +22,11 @@ exports.Library = schema('Library', {
     licenses: refs('License')
 });
 
+// this a bit contrived example illustrates how to use
+// schemas as sort of templates. this schema is needed
+// for testing delete cascade as well
+exports.LibraryAuthor = schema('LicenseAuthor', {
+    library: ref('Library', {required: true}),
+    author: ref('Author', {required: true}),
+    role: String
+});
