105f36ad2715ffc03442e22b5d6f3ea22644e2c3
Juho Vepsalainen <bebraw@gmail.com>
2013-03-10 12:44:01 +0200

    Test getAll

--- tests/tests.js
+++ tests/tests.js
@@ -32,6 +32,7 @@ function main() {
         removeAuthor,
         getAuthor,
         getAuthorName,
+        getAllAuthors,
         getMeta
     ], function(t) {
         return function(cb) {
@@ -83,6 +84,30 @@ function getAuthor(cb) {
             cb(err, d);
         });
     });
+}
+
+function getAllAuthors(cb) {
+    // TODO: move into an array and generate createAuthors based on these
+    var firstData = {name: 'Jack'};
+    var secondData = {name: 'Joe'};
+
+    async.series([
+        createAuthor(firstData),
+        createAuthor(secondData),
+        getAll
+    ], cb);
+
+    function getAll(cb) {
+        sugar.getAll(models.Author, function(err, d) {
+            assert.equal(d.length, 2);
+
+            // TODO: check just if the returned data contains (no need for order)
+            assert.equal(d[0].name, firstData.name);
+            assert.equal(d[1].name, secondData.name);
+
+            cb(err, d);
+        });
+    }
 }
 
 function updateAuthor(cb) {
