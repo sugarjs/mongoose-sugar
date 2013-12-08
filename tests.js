bd594651e381bb0b5b633ccaccb648d907b1ae8b
Juho Vepsalainen <bebraw@gmail.com>
2013-12-08 22:10:37 +0200

    Add address generator
    Closes #15.

--- tests.js
+++ tests.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+var assert = require('assert');
+
 var spec = require('sugar-spec');
 var mongoose = require('mongoose');
 
@@ -8,9 +10,30 @@ var sugar = require('./lib/mongoose-sugar');
 main();
 
 function main() {
+    testParseAddress();
+
     spec({
         sugar: sugar,
         address: 'mongodb://localhost/mongoose-sugar-test',
         engine: mongoose
     });
+}
+
+function testParseAddress() {
+    var simple = {
+        hostname: 'demo',
+        port: 1000,
+        db: 'db'
+    };
+    var withPassword = {
+        hostname: 'host',
+        port: 1234,
+        db: 'test',
+        username: 'user',
+        password: 'pw'
+    };
+
+    console.log(sugar.parseAddress(simple));
+    console.log(sugar.parseAddress(withPassword));
+    console.log();
 }
